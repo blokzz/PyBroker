4f918c7b9d3b383d8cd9579e77d626df56aaa58b
blokz <thebloxpl@gmail.com>
2026-02-15 15:09:00 +0100

    further server implementation

--- server.py
+++ server.py
@@ -4,5 +4,19 @@ import struct
 header_format = "!IB"
 header_size = struct.calcsize(header_format)
 
-def handle_client(writer , reader):
-    
+async def handle_client(writer , reader):
+    address = writer.get_extra_info('peername')
+    print(f"ESTABLISHED CONNECTION WITH {address}")
+    try:
+        while True:
+            header = await reader.read(header_size)
+            mes_length ,command= struct.unpack(header_format ,header)
+
+    except asyncio.IncompleteReadError:
+        print(f"CLIENT {address} DISCONNECTED")
+    except Exception as e:
+        print(f"ERROR: {e}")
+    finally:
+        writer.close()
+        await writer.wait_closed()
+        print(f"CONNECTION WITH {address} CLOSED")
