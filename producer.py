ab328cb1dea8d8ab53b34cbcf11b5c73949b282a
blokz <thebloxpl@gmail.com>
2026-02-20 19:53:24 +0100

    client implementation

--- producer.py
+++ producer.py
@@ -2,4 +2,6 @@ import socket
 import struct
 
 def send_message(sock , command , text):
-    
+    payload = text.encode('utf-8')
+    header = struct.pack("!IB" , len(payload) , command)
+    sock.sendall(header + payload)
